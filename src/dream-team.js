c5ce9e8066118d4228df1117832ca3e9676cc121
Vitali-Hrybouski <60929162+Vitali-Hrybouski@users.noreply.github.com>
2020-03-04 12:43:08 +0300

    feat: add solution task dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,18 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(secretName) {
+
+  let name = false;
+  if (Array.isArray(secretName) === true) {
+    name = "";
+    let arrNames = [];
+    secretName.forEach(function (item) {
+      if (typeof item === "string") {
+        arrNames.push(item.toUpperCase().replace(/[\s{2,}]+/g, '').charAt(0));
+      }
+      arrNames.sort();
+    });
+    name = arrNames.toString().replace(/[\s{2,}]+/g, '');
+
+    if (name === "") name = false;
+  }
+  return name;
 };
