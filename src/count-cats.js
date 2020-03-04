177be00f09e1f9c7c358c54d6f4551afeb4ab180
Vitali-Hrybouski <60929162+Vitali-Hrybouski@users.noreply.github.com>
2020-03-04 11:32:02 +0300

    feat: add solution task count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,23 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i]) === false) {
+      if (typeof arr[i] === "string") {
+        if (arr[i].includes('^^') === true && arr[i].length === 2) {
+          sum += 1;
+        }
+      }
+    }
+    else {
+
+      let test = arr[i].forEach(function (item) {
+        if (typeof item === "string") {
+          if (item.includes('^^') === true && item.length === 2) {
+            sum += 1;
+          }
+        }
+      });
+    }
+  }
+  return sum;
 };
