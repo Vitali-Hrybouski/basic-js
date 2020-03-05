0fa27b7bdff6c69432ce56656456fcf6a48f740c
Vitali-Hrybouski <60929162+Vitali-Hrybouski@users.noreply.github.com>
2020-03-05 13:10:07 +0300

    feat: add solution task transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,38 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+
+    if (Array.isArray(arr) != true) throw 'Not implemented';
+
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] === '--discard-next') {
+
+            if (i + 1 >= arr.length) break;
+            i++;
+            continue;
+        }
+
+        if (arr[i] === '--discard-prev') {
+
+            if (i - 1 < 0) continue;
+            result.pop();
+            continue;
+        }
+
+        if (arr[i] === '--double-next') {
+
+            if (i + 1 >= arr.length) break;
+            result.push(arr[i + 1]);
+            continue;
+        }
+        if (arr[i] === '--double-prev') {
+
+            if (i - 1 < 0) continue;
+            result.push(arr[i - 1]);
+            continue;
+        }
+        result.push(arr[i]);
+    }
+    return result;
+
+}
