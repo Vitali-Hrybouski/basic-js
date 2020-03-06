de3a3938273a2d6d079582767566241f152baf73
Vitali-Hrybouski <60929162+Vitali-Hrybouski@users.noreply.github.com>
2020-03-06 14:17:45 +0300

    feat: add solution task simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,30 @@
 const chainMaker = {
+  chain: new Array(),
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.length;
+    return this;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (value == undefined && value != null) this.chain.push("( )");
+    else this.chain.push('( ' + value + ' )');
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (typeof position != "number" || position < 1 || position > this.chain.length) {
+      this.chain = new Array();
+      throw new Error();
+    }
+    else this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let result = this.chain.join("~~");
+    this.chain = new Array();
+    return result;
   }
 };
-
 module.exports = chainMaker;
