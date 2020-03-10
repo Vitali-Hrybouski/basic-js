f49cf0f46cac190f6db906971a8eacd8633c3856
Vitali-Hrybouski <60929162+Vitali-Hrybouski@users.noreply.github.com>
2020-03-10 14:38:38 +0300

    feat: add solution task extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,30 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
-  
+module.exports = function repeater(str, options) {
+    let arr = [];
+    if (options.separator == undefined) options.separator = "+";
+    if (options.additionSeparator == undefined) options.additionSeparator = "|";
+
+    function addAdditionSeparator() {
+        let arrAddition = [];
+        for (let i = 0; i < options.additionRepeatTimes; i++) {
+            arrAddition.push(String(options.addition));
+
+        }
+        return arrAddition.join(options.additionSeparator);
+    };
+
+    if (options.repeatTimes == undefined) {
+        if (options.additionRepeatTimes == undefined) arr.push(str + String(options.addition));
+        else arr.push(str + addAdditionSeparator());
+    }
+
+    else {
+        for (let i = 0; i < options.repeatTimes; i++) {
+            if (options.additionRepeatTimes == undefined) arr.push(str);
+            else {
+                arr.push(str + addAdditionSeparator());
+            }
+        }
+
+    }
+    return arr.join(options.separator);
+}
