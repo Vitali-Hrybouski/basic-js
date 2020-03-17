8cce280b0fc2c40640b84a4e6b6ecd1df97d56ac
Vitali-Hrybouski <60929162+Vitali-Hrybouski@users.noreply.github.com>
2020-03-17 14:22:37 +0300

    feat: add solution task vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,40 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(type = true) {
+        this.type = type;
+    }
+    encrypt(message, key) {
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        let resultStr = [];
+        for (let i = 0, j = 0; i < message.length; i++) {
+            if (message.charCodeAt(i) > 90 || message.charCodeAt(i) < 65) {
+                resultStr[i] = String.fromCharCode(message.charCodeAt(i));
+            }
+            else {
+                resultStr[i] = String.fromCharCode((((message.charCodeAt(i) - 65) + (key.charCodeAt(j % key.length) - 65)) % 26) + 65);
+                j++;
+            }
+        }
+        if (this.type !== true) return resultStr.reverse().join("");
+        else return resultStr.join("");
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    decrypt(message, key) {
+
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        let resultStr = [];
+        for (let i = 0, j = 0; i < message.length; i++) {
+            if (message.charCodeAt(i) > 90 || message.charCodeAt(i) < 65) {
+                resultStr[i] = String.fromCharCode(message.charCodeAt(i));
+            }
+            else {
+                resultStr[i] = String.fromCharCode((((message.charCodeAt(i) - 65) + 26 - (key.charCodeAt(j % key.length) - 65)) % 26) + 65);
+                j++
+            }
+        }
+        if (this.type !== true) return resultStr.reverse().join("");
+        else return resultStr.join("");
     }
 }
 
